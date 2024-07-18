96bbbd4b7b15ed356118b0af423d2c9a2ab64995
Danny Tuppeny <danny@tuppeny.com>
2024-07-18 19:48:38 +0100

    Revert "Update coverage.sh"
    This reverts commit 588878c939765bcf38e23d0a12ef1d051bbeaee9.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,5 +8,4 @@ dart pub global activate coverage
 firebase emulators:start --project dart-firebase-admin --only firestore,auth &
 
 # dart test --coverage=coverage
-# dart pub run test  --coverage=coverage
-dart pub run test
+dart pub run test  --coverage=coverage
