85f8d8b1d15b2090d8bfb2f611a6c456a40309ee
Danny Tuppeny <danny@tuppeny.com>
2024-07-18 18:32:22 +0100

    Test a failure

---
+++ packages/dart_firebase_admin/bin/throw.dart
@@ -0,0 +1,3 @@
+void main() {
+  throw 'Oops';
+}
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,16 +5,4 @@ set -e -m
 
 dart pub global activate coverage
 
-firebase emulators:start --project dart-firebase-admin --only firestore,auth &
-
-dart run --pause-isolates-on-exit --disable-service-auth-codes --enable-vm-service=8181 test &
-
-jobs -l
-
-dart pub global run coverage:collect_coverage --wait-paused --uri=http://127.0.0.1:8181/ -o coverage/coverage.json --resume-isolates --scope-output=foo &
-
-jobs -l
-
-fg %2
-
-
+firebase emulators:exec --project dart-firebase-admin --only firestore,auth "dart run bin/throw.dart"
