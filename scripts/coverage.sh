b0002de6686b7b6fe510c562ee911f60d93b0403
Danny Tuppeny <danny@tuppeny.com>
2024-07-18 12:22:51 +0100

    Update coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,7 +5,7 @@ set -e
 
 dart pub global activate coverage
 
-firebase emulators:start --project dart-firebase-admin --only firestore,auth
+firebase emulators:start --project dart-firebase-admin --only firestore,auth &
 
 dart run --pause-isolates-on-exit --disable-service-auth-codes --enable-vm-service=8181 test &
 
@@ -14,6 +14,7 @@ jobs -l
 dart pub global run coverage:collect_coverage --wait-paused --uri=http://127.0.0.1:8181/ -o coverage/coverage.json --resume-isolates --scope-output=foo
 
 jobs -l
-fg %1
+
+fg %2
 
 
