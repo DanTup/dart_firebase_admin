22cd532b5656a0cb329a79774d6ae6e1caefb144
Danny Tuppeny <danny@tuppeny.com>
2024-07-18 12:06:46 +0100

    Try seperate commands

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,6 +5,6 @@ set -e
 
 dart pub global activate coverage
 
-firebase emulators:exec --project dart-firebase-admin --only firestore,auth "dart test --coverage=coverage" 
+firebase emulators:exec --project dart-firebase-admin --only firestore,auth "dart run --pause-isolates-on-exit --disable-service-auth-codes --enable-vm-service=8181 test & dart pub global run coverage:collect_coverage --wait-paused --uri=http://127.0.0.1:8181/ -o coverage/coverage.json --resume-isolates --scope-output=foo"
 
 format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.dart_tool/package_config.json --report-on=lib
