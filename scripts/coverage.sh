3d37220dc2d3a0b1d56061feb44dfe7bbe128904
Danny Tuppeny <danny@tuppeny.com>
2024-07-18 12:14:52 +0100

    Seperate running emulators from script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,6 +5,15 @@ set -e
 
 dart pub global activate coverage
 
-firebase emulators:exec --project dart-firebase-admin --only firestore,auth "dart run --pause-isolates-on-exit --disable-service-auth-codes --enable-vm-service=8181 test & dart pub global run coverage:collect_coverage --wait-paused --uri=http://127.0.0.1:8181/ -o coverage/coverage.json --resume-isolates --scope-output=foo"
+firebase emulators:start --project dart-firebase-admin --only firestore,auth
 
-format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.dart_tool/package_config.json --report-on=lib
+dart run --pause-isolates-on-exit --disable-service-auth-codes --enable-vm-service=8181 test &
+
+jobs -l
+
+dart pub global run coverage:collect_coverage --wait-paused --uri=http://127.0.0.1:8181/ -o coverage/coverage.json --resume-isolates --scope-output=foo
+
+jobs -l
+fg %1
+
+
