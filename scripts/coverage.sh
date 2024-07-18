588878c939765bcf38e23d0a12ef1d051bbeaee9
Danny Tuppeny <danny@tuppeny.com>
2024-07-18 19:31:11 +0100

    Update coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,4 +8,5 @@ dart pub global activate coverage
 firebase emulators:start --project dart-firebase-admin --only firestore,auth &
 
 # dart test --coverage=coverage
-dart pub run test  --coverage=coverage
+# dart pub run test  --coverage=coverage
+dart pub run test
